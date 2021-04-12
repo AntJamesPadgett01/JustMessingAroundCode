9fbdcf8403db133020f48a4b581f3264c1da596e
ArmyAntMF54 <anthonyjpadgett@gmail.com>
2021-04-12 13:30:44 -0500

    Just Joshing around

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace JustMessingAroundCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Whats your name?");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"Hello {userName}, How old are you?");
+            var userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Sooo {userName} you are {userAge} years old?");
+
+            if (userAge < 21)
+            {
+                Console.WriteLine("You are to young to ride this ride");
+            }
+            else
+            {
+                Console.WriteLine("You are welcome to come in");
+            }
+        }
+    }
+}
